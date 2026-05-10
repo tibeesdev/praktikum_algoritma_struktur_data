d38eaf073959cd4fcca82d45f4d75f0ba8ae22e8
VM <tibeesdev@gmail.com>
2026-05-10 00:42:32 -0700

    tambah quicksort

--- modul 5/modul5.cpp
+++ modul 5/modul5.cpp
@@ -3,13 +3,14 @@
 
 using namespace std;
 
-
+// insertion
 void insertionSort(){
     string nama;
     cout << "Masukkan Nama: ";
     getline (cin, nama);
 
-    int n = nama.length();
+    if (!nama.empty()){
+        int n = nama.length();
 
     for (int i = 1; i < n;i++){
         char key = nama[i];
@@ -21,9 +22,15 @@ void insertionSort(){
         }
         nama[j + 1] = key;
     }
+
     cout << "Hasil Insertion Sort Nama: " << nama << endl;
+
+    } else {
+        cout << "Nama Tidak Boleh Kosong!"<< endl;
+    }
 }
 
+//merge
 void merge(string &nama, int left, int mid, int right){
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -79,10 +86,81 @@ void mergeCall(){
     }
 }
 
+//shell
+void shellSort(){
+    string nama;
+    cout << "Masukkan Nama: ";
+    getline (cin, nama);
+
+    if(!nama.empty()){
+        int n = nama.length();
+
+        for (int gap = n/2; gap >0 ; gap /= 2){
+            for (int i = gap; i < n; i++){
+                char temp = nama[i];
+                int j;
+
+                for (j = i; j >= gap && nama[j - gap] > temp; j -= gap){
+                    nama[j] = nama[j - gap];
+                }
+                nama[j] = temp;
+            }
+        }
+
+        cout << "Hasil Shell Sort Nama: " << nama << endl;
+    } else {
+        cout << "Nama Tidak Boleh Kosong!" << endl;
+    }
+}
+
+// quicksort
+void swap(char &a, char &b){
+    char temp = a;
+    a= b;
+    b = temp;
+}
+
+int partition(string &nim, int low, int high){
+    char pivot = nim [high];
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++){
+        if (nim[j] <= pivot){
+            i++;
+            swap(nim[i], nim[j]);
+        }
+    }
+    swap(nim[i+1], nim[high]);
+    return (i+1);
+}
+
+void quickSort(string &nim, int low, int high){
+    if (low < high){
+        int pi = partition(nim, low, high);
+
+        quickSort(nim, low, pi-1);
+        quickSort(nim, pi + 1, high);
+    }
+}
+
+void quickSortCall(){
+    string nim;
+    cout << "Masukkan NIM: ";
+    getline (cin, nim);
+
+    if (!nim.empty()){
+        quickSort(nim, 0, nim.length() - 1);
+        cout << "Hasl Quick Sort NIM: "<< nim<< endl;
+    } else {
+        cout << "NIM Tidak Boleh Kosong!" <<endl;
+    }
+}
 
 int main(){
     //insertionSort();
-    mergeCall();
+    //mergeCall();
+    //shellSort();
+    quickSortCall();
 
 }
 
